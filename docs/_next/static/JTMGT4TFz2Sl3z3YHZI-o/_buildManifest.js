22e51aad97b42e93f13c5d9781be7db9e62fc053
KazuyaNagaki <k.nagaki.sys23@morijyobi.ac.jp>
2024-08-20 12:22:23 +0900

    再変更

--- docs/_next/static/JTMGT4TFz2Sl3z3YHZI-o/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/61-eab2cdcdf8cec91e.js","static/chunks/pages/index-155b2a323bfc8827.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
