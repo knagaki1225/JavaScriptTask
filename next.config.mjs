40c1efeddd7dea01c7180a2d825d98bbb669cf3b
KazuyaNagaki <k.nagaki.sys23@morijyobi.ac.jp>
2024-08-20 12:14:35 +0900

    変更

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   distDir: 'docs',
+  basePath: "/JavaScriptTask", // リポジトリ名を指定
+  assetPrefix: "/JavaScriptTask/", // 同じくリポジトリ名を指定
 };
 
 export default nextConfig;
